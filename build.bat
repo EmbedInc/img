f231059bdf610c13163667aabe43cedb0214f20d
Olin Lathrop <OlinLathrop@users.noreply.github.com>
2019-03-26 07:14:00 -0400

    Updated to new build script conventions.

--- build.bat
+++ build.bat
@@ -1,11 +1,8 @@
 @echo off
 rem
-rem   BUILD [-dbg]
-rem
-rem   Build everything in this source directory, which is the IMG library.
+rem   Build everything from this source directory.
 rem
 setlocal
-set srclib=imglib
-call godir (cog)source/%srclib%
-call build_jpeg_lib
-call build_img_lib
+call godir "(cog)source/imglib"
+
+call build_lib
--- build_jpeg_lib.bat
+++ build_jpeg_lib.bat
@@ -1,69 +1,69 @@
 @echo off
 rem
-rem   Build the STUFF library.
+rem   Build the JPEG library.
 rem
 setlocal
+call build_pasinit
 set libname=jpeg
-set srclib=imglib
 
-call src_get %srclib% jchuff.h
-call src_get %srclib% jconfig.h
-call src_get %srclib% jdct.h
-call src_get %srclib% jdhuff.h
-call src_get %srclib% jerror.h
-call src_get %srclib% jinclude.h
-call src_get %srclib% jmemsys.h
-call src_get %srclib% jmorecfg.h
-call src_get %srclib% jpegint.h
-call src_get %srclib% jpeglib.h
-call src_get %srclib% jversion.h
+call src_get %srcdir% jchuff.h
+call src_get %srcdir% jconfig.h
+call src_get %srcdir% jdct.h
+call src_get %srcdir% jdhuff.h
+call src_get %srcdir% jerror.h
+call src_get %srcdir% jinclude.h
+call src_get %srcdir% jmemsys.h
+call src_get %srcdir% jmorecfg.h
+call src_get %srcdir% jpegint.h
+call src_get %srcdir% jpeglib.h
+call src_get %srcdir% jversion.h
 
-call src_c %srclib% %libname%_capimin %1
-call src_c %srclib% %libname%_capistd %1
-call src_c %srclib% %libname%_ccoefct %1
-call src_c %srclib% %libname%_ccolor %1
-call src_c %srclib% %libname%_cdctmgr %1
-call src_c %srclib% %libname%_chuff %1
-call src_c %srclib% %libname%_cinit %1
-call src_c %srclib% %libname%_cmainct %1
-call src_c %srclib% %libname%_cmarker %1
-call src_c %srclib% %libname%_cmaster %1
-call src_c %srclib% %libname%_comapi %1
-call src_c %srclib% %libname%_cparam %1
-call src_c %srclib% %libname%_cphuff %1
-call src_c %srclib% %libname%_cprepct %1
-call src_c %srclib% %libname%_csample %1
-call src_c %srclib% %libname%_ctrans %1
-call src_c %srclib% %libname%_dapimin %1
-call src_c %srclib% %libname%_dapistd %1
-call src_c %srclib% %libname%_datadst %1
-call src_c %srclib% %libname%_datasrc %1
-call src_c %srclib% %libname%_dcoefct %1
-call src_c %srclib% %libname%_dcolor %1
-call src_c %srclib% %libname%_ddctmgr %1
-call src_c %srclib% %libname%_dhuff %1
-call src_c %srclib% %libname%_dinput %1
-call src_c %srclib% %libname%_dmainct %1
-call src_c %srclib% %libname%_dmarker %1
-call src_c %srclib% %libname%_dmaster %1
-call src_c %srclib% %libname%_dmerge %1
-call src_c %srclib% %libname%_dphuff %1
-call src_c %srclib% %libname%_dpostct %1
-call src_c %srclib% %libname%_dsample %1
-call src_c %srclib% %libname%_dtrans %1
-call src_c %srclib% %libname%_error %1
-call src_c %srclib% %libname%_fdctflt %1
-call src_c %srclib% %libname%_fdctfst %1
-call src_c %srclib% %libname%_fdctint %1
-call src_c %srclib% %libname%_idctflt %1
-call src_c %srclib% %libname%_idctfst %1
-call src_c %srclib% %libname%_idctint %1
-call src_c %srclib% %libname%_idctred %1
-call src_c %srclib% %libname%_img_glue %1
-call src_c %srclib% %libname%_memmgr %1
-call src_c %srclib% %libname%_memnobs %1
-call src_c %srclib% %libname%_quant1 %1
-call src_c %srclib% %libname%_quant2 %1
-call src_c %srclib% %libname%_utils %1
+call src_c %srcdir% %libname%_capimin %1
+call src_c %srcdir% %libname%_capistd %1
+call src_c %srcdir% %libname%_ccoefct %1
+call src_c %srcdir% %libname%_ccolor %1
+call src_c %srcdir% %libname%_cdctmgr %1
+call src_c %srcdir% %libname%_chuff %1
+call src_c %srcdir% %libname%_cinit %1
+call src_c %srcdir% %libname%_cmainct %1
+call src_c %srcdir% %libname%_cmarker %1
+call src_c %srcdir% %libname%_cmaster %1
+call src_c %srcdir% %libname%_comapi %1
+call src_c %srcdir% %libname%_cparam %1
+call src_c %srcdir% %libname%_cphuff %1
+call src_c %srcdir% %libname%_cprepct %1
+call src_c %srcdir% %libname%_csample %1
+call src_c %srcdir% %libname%_ctrans %1
+call src_c %srcdir% %libname%_dapimin %1
+call src_c %srcdir% %libname%_dapistd %1
+call src_c %srcdir% %libname%_datadst %1
+call src_c %srcdir% %libname%_datasrc %1
+call src_c %srcdir% %libname%_dcoefct %1
+call src_c %srcdir% %libname%_dcolor %1
+call src_c %srcdir% %libname%_ddctmgr %1
+call src_c %srcdir% %libname%_dhuff %1
+call src_c %srcdir% %libname%_dinput %1
+call src_c %srcdir% %libname%_dmainct %1
+call src_c %srcdir% %libname%_dmarker %1
+call src_c %srcdir% %libname%_dmaster %1
+call src_c %srcdir% %libname%_dmerge %1
+call src_c %srcdir% %libname%_dphuff %1
+call src_c %srcdir% %libname%_dpostct %1
+call src_c %srcdir% %libname%_dsample %1
+call src_c %srcdir% %libname%_dtrans %1
+call src_c %srcdir% %libname%_error %1
+call src_c %srcdir% %libname%_fdctflt %1
+call src_c %srcdir% %libname%_fdctfst %1
+call src_c %srcdir% %libname%_fdctint %1
+call src_c %srcdir% %libname%_idctflt %1
+call src_c %srcdir% %libname%_idctfst %1
+call src_c %srcdir% %libname%_idctint %1
+call src_c %srcdir% %libname%_idctred %1
+call src_c %srcdir% %libname%_img_glue %1
+call src_c %srcdir% %libname%_memmgr %1
+call src_c %srcdir% %libname%_memnobs %1
+call src_c %srcdir% %libname%_quant1 %1
+call src_c %srcdir% %libname%_quant2 %1
+call src_c %srcdir% %libname%_utils %1
 
-call src_lib %srclib% %libname%
+call src_lib %srcdir% %libname%
